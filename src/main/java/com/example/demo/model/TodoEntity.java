b9f9f4ad8ecc543a107d0e58e6154cd4e4d4917c
Nam Ye Eun <wiseslope22@sookmyung.ac.kr>
2023-06-19 13:21:53 +0900

    백엔드 개발 18. @NamedQuery 사용하기

--- src/main/java/com/example/demo/model/TodoEntity.java
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -3,10 +3,9 @@ package com.example.demo.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-
 import org.hibernate.annotations.GenericGenerator;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,6 +17,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 @Table(name = "Todo")
+@NamedQuery(name = "TodoRepository.searchByUserId", query = "select t from TodoEntity t where t.userId = ?1")
 public class TodoEntity {
 	@Id
 	@GeneratedValue(generator = "system-uuid") // 자동으로 id 성성
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -2,7 +2,6 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
 
@@ -12,13 +11,13 @@ public class TodoService {
 	private TodoRepository repository;
 
 public String testService(){
-	// Todo Entity 생성
-	TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
-	// Todo Entity 저장
-	repository.save(entity);
-	// Todo Entity 검색
-	TodoEntity savedEntity = repository.findByUserId(entity.getUserId()).get(0);
-	return savedEntity.getUserId();
-	}
-
+// Todo Entity 생성
+TodoEntity entity = TodoEntity.builder().userId("Nam Ye Eun").title("My first todo item").build();
+//Todo Entity 저장
+repository.save(entity);
+//Todo Entity 검색
+TodoEntity savedEntity =
+repository.searchByUserId(entity.getUserId()).get(0);
+return savedEntity.getUserId();
+}
 }
